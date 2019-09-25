30112152ca5d6578adeaa0144d4cacc0f9b4d9d7
Valentyna777 <valiuja777@gmail.com>
2019-09-25 14:44:29 +0300

    First Commit

--- git/homework.py
+++ git/homework.py
@@ -15,7 +15,7 @@ def is_two_object_has_same_value(first: Any, second: Any) -> bool:
     If @first and @second has same value should return True
     In another case should return False
     """
-    pass
+    return first == second
 
 
 def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
@@ -23,7 +23,7 @@ def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    return type(first) == type(second)
 
 
 def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
@@ -31,7 +31,7 @@ def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    return id(first) == id(second)
 
 
 def multiple_ints(first_value: int, second_value: int) -> int:
@@ -48,7 +48,11 @@ def multiple_ints(first_value: int, second_value: int) -> int:
     Returns:
         Product of elements
     """
-    pass
+
+    if isinstance(first_value, int) and isinstance(second_value, int):
+            return first_value * second_value
+    else:
+            raise ValueError
 
 
 def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
@@ -78,7 +82,13 @@ def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
             print("Not valid input data")
         >>> "Not valid input data"
     """
-    pass
+    try:
+        return int(first_value) * int(second_value)
+    except TypeError:
+        raise ValueError("Not valid input data")
+
+
+
 
 
 def is_word_in_text(word: str, text: str) -> bool:
@@ -97,14 +107,26 @@ def is_word_in_text(word: str, text: str) -> bool:
         >>> False
 
     """
-    pass
+    return word in text
 
 
 def some_loop_exercise() -> list:
     """
     Use loop to create list that contain int values from 0 to 12 except 6 and 7
     """
-    pass
+    my_list = []
+    i = -1
+    while True:
+        i += 1
+        if i >= 13:
+            break
+        if i == 6:
+            continue
+        if i == 7:
+            continue
+        my_list.append(i)
+    return my_list
+
 
 
 def remove_from_list_all_negative_numbers(data: List[int]) -> list:
@@ -116,7 +138,12 @@ def remove_from_list_all_negative_numbers(data: List[int]) -> list:
         remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
         >>> [1, 5, 8]
     """
-    pass
+    new_list = []
+    for i in data:
+        if i < 0:
+            continue
+        new_list.append(i)
+    return new_list
 
 
 def alphabet() -> dict:
@@ -127,7 +154,12 @@ def alphabet() -> dict:
         alphabet()
         >>> {"a": 1, "b": 2 ...}
     """
-    pass
+    alph = list('abcdefghijklmnopqrstuvwxyz')
+    numbers = []
+    for i in range(1, 27):
+        numbers.append(i)
+    new_dict = dict(zip(numbers, alph))
+    return new_dict
 
 
 def simple_sort(data: List[int]) -> List[list]:
@@ -139,4 +171,5 @@ def simple_sort(data: List[int]) -> List[list]:
     Returns:
 
     """
-    pass
+    sorted_list = sorted(data)
+    return sorted_list
