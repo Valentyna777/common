af7dbca8a2744196e20f829535d30173e5494299
Valentyna777 <valiuja777@gmail.com>
2019-11-07 13:37:50 +0200

    Advanced Flask

--- Advanced_Flask/products/get_data_from_json.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def get_prod_data():
-    with open("products/product_list.json") as file:
-        return json.load(file)
--- Advanced_Flask/supermarkets/get_data_from_json.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def get_super_data():
-    with open("supermarkets/supermarket_list.json") as file:
-        return json.load(file)
